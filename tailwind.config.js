5f5a509aa660c963f1873ed61ca4a331b5148d86
spin~ <public@spin.rip>
2023-04-18 18:54:05 -0700

    Initial commit, not complete yet (WIP)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      transitionProperty: {
+        'bg': 'background-color',
+      },
+      animation: {
+        text: 'text 5s ease infinite',
+      },
+      keyframes: {
+        text: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+};
