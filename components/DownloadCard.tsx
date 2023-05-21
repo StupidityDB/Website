962f141d3ee6e31cf54f4d292ad077a087b04e93
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-05-21 17:10:51 +0300

    remove white theme because nobody likes

--- components/DownloadCard.tsx
+++ components/DownloadCard.tsx
@@ -11,17 +11,17 @@ interface DownloadCardProps {
 
 export default function DownloadCard({ title, description, downloadLink, websiteLink, icon_url }: DownloadCardProps): JSX.Element {
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
+    <div className="w-full max-w-sm border rounded-lg shadow bg-gray-800 border-gray-700 p-4">
 
       <div className="flex flex-col items-center pb-10">
         <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={icon_url} alt={title + ' image'}/>
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{title}</h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400 text-center">{description}</span>
+        <h5 className="mb-1 text-xl font-medium text-white">{title}</h5>
+        <span className="text-sm text-gray-400 text-center">{description}</span>
         <div className="flex mt-4 space-x-3 md:mt-6">
           {
-            websiteLink && <a href={websiteLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">{title} Website</a>
+            websiteLink && <a href={websiteLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center border rounded-lg focus:ring-4 focus:outline-none bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-700 focus:ring-gray-700">{title} Website</a>
           }
-          <a href={downloadLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Download ReviewDB</a>
+          <a href={downloadLink} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Download ReviewDB</a>
         </div>
       </div>
     </div>
