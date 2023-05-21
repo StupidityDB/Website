6c4292db2f0c11fd801c0c1b5d3343836c7ae79b
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-05-21 16:15:21 +0300

    fix homepage on mobile

--- app/Introduction.tsx
+++ app/Introduction.tsx
@@ -5,9 +5,9 @@ const Introduction: React.FC = (): JSX.Element => (
   <div className='flex flex-col gap-3 items-center justify-between mt-16'>
     <h2 className='gg-semibold text-4xl'>ReviewDB</h2>
     <p className='gg-normal text-xl text-center'>Leave reviews on other users&apos; Discord profiles!<br />Anyone with a ReviewDB plugin on any Discord client mod will be able to see your review.</p>
-    <div className='w-full flex items-center justify-center' >
-      <Link className='button mt-2 lg:mt-4 lg:w-1/5 text-center' href='/dashboard'>Dashboard</Link>
-      <Link className='button mt-2 lg:mt-4 lg:w-1/5 ml-4 text-center' href='/download'>Download ReviewDB</Link>
+    <div className='w-full flex justify-center gap-5' >
+      <Link className='button mt-2 lg:mt-4 md:w-1/5 text-center flex-1 md:flex-none items-center flex justify-center' href='/dashboard'>Dashboard </Link>
+      <Link className='button mt-2 lg:mt-4 md:w-1/5 text-center flex-1 md:flex-none items-center flex justify-center' href='/download'>Download ReviewDB</Link>
     </div>
   </div>
 )
